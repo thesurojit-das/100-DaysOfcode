827251752890bd314d42a7283068650eddafada9
Surojit <81943886+thesurojit-das@users.noreply.github.com>
2022-02-03 13:10:09 +0530

    Add files via upload

---
+++ Create_Object.java
@@ -0,0 +1,11 @@
+
+public class Create_Object {
+	public void show() {
+		// TODO Auto-generated method stub
+		System.out.print("Creating Object Done");
+	}
+public static void main(String[] args) {
+	Create_Object object=new Create_Object();
+	object.show();
+}
+}
