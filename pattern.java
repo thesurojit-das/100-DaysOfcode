cd44fc5382acbbf12d2c01474715f1a84c382911
Surojit <81943886+thesurojit-das@users.noreply.github.com>
2022-02-19 23:45:08 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class pattern {
+public static void main(String[] args) {
+	int n = 10;
+	for(int i=0;i<n;i++)
+	{
+		for(int i1=0;i1<n-i;i1++)
+		{
+			System.out.print(" # ");
+		}
+		System.out.println();
+	}
+}
+}
